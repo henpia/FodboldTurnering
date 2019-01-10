a3c455970e80286aa958794be6ab250b5c85f6a2
Henrik Christiansen <henpia@gmail.com>
2019-01-10 18:18:47 +0100

    Oprettelse af Context class.

---
+++ FT.DAL/Repositories/FodboldTurneringDB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using FT.Entities.Models;
+
+namespace FT.DAL.Repositories
+{
+    public class FodboldTurneringDB : DbContext
+    {
+        public DbSet<Turnering> Turneringer { get; set; }
+        public DbSet<Hold> Hold { get; set; }
+        public DbSet<Kamp> Kampe { get; set; }
+        public DbSet<Runde> Runder { get; set; }
+    }
+}
