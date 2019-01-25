32388e5f8dc9e2f50647fb955eb855c9185a99d9
Henrik Christiansen <henpia@gmail.com>
2019-01-25 11:46:30 +0100

    Upgrade to Bootstrap 4.
    Opgradering til Bootstrap 4. Ændring af navbar til at benytte BS4. Ændring af tabeller i Turnerings Details view til BS4.

--- FT.WEB/App_Start/BundleConfig.cs
+++ FT.WEB/App_Start/BundleConfig.cs
@@ -20,7 +20,7 @@ namespace FT.WEB
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                        "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
